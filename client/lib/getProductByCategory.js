af9c820546773363e3de32c1efd7ebcce5c3146c
Erasmus <refugedesigns17@gmail.com>
2023-06-02 01:16:37 +0000

    fixed bugs

--- client/lib/getProductByCategory.js
+++ client/lib/getProductByCategory.js
@@ -2,7 +2,7 @@ export const getProductByCategory = async(category) => {
 
     let products 
     try {
-        const response = await fetch(`http://localhost:8000/api/v1/products?filterItems=${category}&limit=20`)
+        const response = await fetch(`/api/v1/products?filterItems=${category}&limit=20`)
         const result = await response.json()
     
         products = result.products.map(product => ({...product, link: `/${product._id}`}))
